cb58ceed83bf2b30b6f8be09f92a34a13ffb648f
karanveerss383 <karanveerss383@gmail.com>
2026-04-14 11:33:21 -0500

    add 3 helper functions for cleaning

--- src/helpers.py
+++ src/helpers.py
@@ -4,3 +4,44 @@ Helper functions for [Your Project Name].
 This module contains reusable functions for data cleaning,
 analysis, and visualization.
 """
+
+def fill_nan(data, columns_values:dict):
+
+    """
+    This function takes in a dataframe and dictionary of columns with the values to fill in place of 'NaN'
+    and returns a dataframe
+    """
+
+    for i in columns_values.keys():
+        data[i] = data[i].fillna(columns_values[i])
+    return data
+
+def title_case(data, columns=[]):
+    
+    """
+    This funtion takes in a dataframe and columns list, to change the format to title-case, to improve readability
+    and returns dataframe
+    """
+    
+    if (columns == []):
+        columns = data.columns
+    for column in columns:
+        if (data[column].dtype == 'object'):
+            data[column] = data[column].str.title()
+    return data
+
+def datetimecols(data, columns, column_name):
+
+    """"
+    This funtion takes in a dataframe and the date and time columns list, and a name for combined datetime column
+    and joins the two columns together, converts them to datetime. Also drops the original date and time separate columns.
+    returing the final dataframe
+    """
+
+
+    if (len(columns) != 2):
+        raise IndexError (f"expected 2 columns got {len(columns)}")
+    data[column_name] = data[columns[0]] + ' ' + data[columns[1]]
+    data[column_name] = pd.to_datetime(data[column_name])
+    data = data.drop(columns=columns, axis=1)
+    return data
